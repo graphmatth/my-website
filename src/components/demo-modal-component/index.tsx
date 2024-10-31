1c6f6d2ff07aaef970ffe1994a6a2dafa1e9b989
Matthieu Caron <contact@matthieucaron.fr>
2024-10-31 11:34:21 +0100

    chore: add first draft goodmed website content with astro islands

---
+++ src/components/demo-modal-component/index.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import "./style-modal.css";
+
+const ModalComponent = () => {
+  return (
+    <div
+      style={{
+        border: "2px dashed var(--border-color-card)",
+        padding: "32px 8px",
+        borderRadius: 8,
+        backgroundColor: "var(--background-card-modal)",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Dialog.Root>
+        <div className="video--container">
+          <Dialog.Trigger asChild>
+            <div className="modal-trigger">
+              <div
+                className="overlay-video"
+                style={{ width: "100%", height: "100%" }}
+              />
+              <video autoPlay muted playsInline loop className="video">
+                <source
+                  src="https://static.test.c-napps.com/marketing/VideoGoodmedFamily-Short.mp4"
+                  type="video/mp4"
+                />
+              </video>
+            </div>
+          </Dialog.Trigger>
+        </div>
+        <Dialog.Trigger asChild>
+          <button className="button-modal">→ Watch the full video</button>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="dialog-overlay" />
+          <Dialog.Content className="dialog-content">
+            <div
+              style={{
+                position: "relative",
+                borderRadius: 12,
+                overflow: "hidden",
+              }}
+            >
+              {" "}
+              <video autoPlay loop className="video video--modal">
+                <source
+                  src="https://static.test.c-napps.com/marketing/VideoGoodmedFamily-Short.mp4"
+                  type="video/mp4"
+                />
+              </video>
+            </div>
+            <Dialog.Close asChild>
+              <button
+                style={{
+                  position: "absolute",
+                  top: -32,
+                  right: 0,
+                  color: "white",
+                  cursor: "pointer",
+                }}
+                className="IconButton"
+                aria-label="Close"
+              >
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </div>
+  );
+};
+
+export default ModalComponent;
